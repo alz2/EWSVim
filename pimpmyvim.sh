fe73d07f90c52152d26f47f9a21d30339c477e84
Andrew Zhang <alz2@illinois.edu>
2017-11-13 14:03:47 -0600

    i needa test this

--- pimpmyvim.sh
+++ pimpmyvim.sh
@@ -2,15 +2,17 @@
 echo "Pimping your vim... "
 
 # get ncurses
+echo "GETTING NCURSES 6"
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz
 # untar
 tar -xzf ncurses-6.0.tar.gz
-mkdir ~/EWSVim
 cd ncurses-6.0/
 ./configure --prefix=$HOME/EWSVim/ncurses
 make && make install
 
 # install Vim
+cd ..
+echo "GETTING VIM 8"
 wget ftp://ftp.home.vim.org/pub/vim/unix/vim-8.0.tar.bz2
 tar -xzf vim-8.0.tar.bz2
 cd vim80
@@ -20,4 +22,5 @@ export LDFLAGS="-L$HOME/EWSVim/ncurses/lib"
 make && make install
 
 # prepend vim 8 to $PATH so vim8 is default
+echo "SETTING VIM 8 AS DEFAULT"
 echo "PATH=$HOME/EWSVim/vim/bin:$PATH" >> ~/.bashrc
